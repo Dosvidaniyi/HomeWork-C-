9e65fe88ca4a35a67cc7dc4e5836e11e39a3e110
Dosvidaniya <kuroglosasha94@mail.ru>
2022-07-02 15:57:14 +0300

    complete

---
+++ DZ1/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b) Console.WriteLine($"max={a}");
+// else Console.WriteLine($"max={b}");
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (max < b) max = b;
+// if (max < c) max = c;
+
+// Console.WriteLine($"max={max}");
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Введите целое число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 % 2 == 1)
+// {
+//     Console.WriteLine("Введенное Вами число нечетное");
+// }
+// else
+// {
+//     Console.WriteLine("Введенное Вами число четное");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите целое число N ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+String result = string.Empty;
+
+for (int i = 2; i <= num1; i += 2)
+{
+    result = result + " " + i;
+}
+Console.WriteLine("Четные числа от 0 до N: " +result);
