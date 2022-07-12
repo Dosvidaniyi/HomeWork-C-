12aa9c520e2b77b105fdafdfe4168a4ac1df5950
Dosvidaniya <kuroglosasha94@mail.ru>
2022-07-12 19:53:58 +0300

    Save

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -65,34 +65,34 @@
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-Console.WriteLine("Введите N: ");
-int num = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите N: ");
+// int num = int.Parse(Console.ReadLine());
 
-int[] FillArray(int num)
-{
-    int[] array = new int[num];
-    int length = array.Length;
-    int index = 0;
-    while (index < length)
-    {
-        array[index] = new Random().Next(0, 2);
-        index++;
-    }
-    return array;
-}
+// int[] FillArray(int num)
+// {
+//     int[] array = new int[num];
+//     int length = array.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         array[index] = new Random().Next(0, 2);
+//         index++;
+//     }
+//     return array;
+// }
 
-void PrintArray(int[] col)
-{
-    Console.Write("[");
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
-    {
-        Console.Write($"{col[position]}, ");
-        position++;
-    }
-    Console.Write("]");
-}
+// void PrintArray(int[] col)
+// {
+//     Console.Write("[");
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.Write($"{col[position]}, ");
+//         position++;
+//     }
+//     Console.Write("]");
+// }
 
-int[] arr = FillArray(num);
-PrintArray(arr);
+// int[] arr = FillArray(num);
+// PrintArray(arr);
---
+++ Task29/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов, 
+// заполненный псевдослучайными числами и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+// Сделал универсально сооздаваемый массив
+
+// Console.WriteLine("Введите длинну массива: ");
+// int num = int.Parse(Console.ReadLine());
+// int[] FillArray(int num)
+// {
+//     int[] array = new int[num];
+//     Random rand = new Random();
+//     int length = array.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         array[index] = rand.Next(0, 99);
+//         index++;
+//     }
+//     return array;
+// }
+// void PrintArray(int[] col)
+// {
+//     Console.Write("[");
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.Write($"{col[position]}, ");
+//         position++;
+//     }
+//     Console.Write("]");
+// }
+
+// int[] arr = FillArray(num);
+// PrintArray(arr);
